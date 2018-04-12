111adfe15a49a87da29779c34cd998fabff1f4e7
Wesley Hanson <wrh1600@gmail.com>
2018-04-11 23:42:59 -0700

    Working on buy functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var colors = require("colors");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -11,56 +12,85 @@ var connection = mysql.createConnection({
     database: "bamazonDB"
 });
 
+var purchase;
 
-inquirer.prompt({
-    type: "list",
-    message: "What would you like to view?",
-    name: "answer",
-    choices: ["All inventory", "Electronics", "Furniture", "Apparel", "Camping/Outdoors", "Tools", "Exit"]
-}).then(function(data){
-    switch(data.answer) {
-        case "All inventory":
-            showItems();
-            break;
 
-        case "Electronics":
-            showElectronics();
-            break;
+function start(){
+    inquirer.prompt({
+        type: "list",
+        message: "What would you like to view?",
+        name: "answer",
+        choices: ["All inventory", "Electronics", "Furniture", "Apparel", "Camping/Outdoors", "Tools", "Exit"]
+    }).then(function(data){
+        switch(data.answer) {
+            case "All inventory":
+                showItems();
+                break;
 
-        case "Furniture":
-            showfurniture();
-            break;
-        
-        case "Apparel":
-            showApparel();
-            break;
+            case "Electronics":
+                showElectronics();
+                break;
+
+            case "Furniture":
+                showfurniture();
+                break;
             
-        case "Camping/Outdoors":
-            showCampingOutdoors();
-            break;
-        
-        case "Tools":
-            showTools();
-            break;
+            case "Apparel":
+                showApparel();
+                break;
+                
+            case "Camping/Outdoors":
+                showCampingOutdoors();
+                break;
             
-        case "Exit":
-            console.log("\nThanks for shopping with Bamazon!\n")
-            setTimeout(function(){
-                connection.end();
-            }, 1500);
-           
-            break;    
-    }
-})
+            case "Tools":
+                showTools();
+                break;
+                
+            case "Exit":
+                console.log("\nThanks for shopping with Bamazon!\n")
+                setTimeout(function(){
+                    connection.end();
+                }, 1500);
+            
+                break;    
+        }
+    })
+}    
+
+showItems();
 
 function showItems() {
     connection.query("SELECT * FROM products", function (err, result, fields) {
         if (err) throw err;
+        var productId = [];
         for(i = 0; i < result.length; i++){
-        console.log("\nItem: " + result[i].product_name + "\n" + "Department: " + result[i].department + "\n" + "Price: $" + result[i].price + "\nStock: " + result[i].stock_qty + "\n----------------");
+            console.log("\n" + colors.green(result[i].id) + ") " + "Item: ".yellow + result[i].product_name  + "\n    Department: ".yellow + result[i].department + "\n    Price: ".yellow + "$"+ result[i].price + "\n-------------------".blue);
+            productId.push(result[i].id);
         }
-      });
-    connection.end();
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Which item would you like to purchase?",
+                choices: productId,
+                name: "purchase"
+            },
+            {
+                type: "input",
+                message: "How many do you want to purchase?",
+                name: "count",
+                // validate: function(value) {
+                //     if (isNaN(value) === false) {
+                //       return true;
+                //     }
+                //     return false;
+                //   }
+            }
+        ]).then(function(data){
+
+        })
+        // console.log(productId[9]);
+    });
 }
 
 function showElectronics() {
@@ -112,3 +142,10 @@ function showTools() {
       });
     connection.end();
 }
+
+function checkStock(){
+    connection.query("SELECT * FROM products.stock_qty", function (err, result, fields){
+        console.log(result);
+    })
+}
+
