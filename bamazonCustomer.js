b6844ad7018723e67eb16bb2deb098db8ec3ea56
Wesley Hanson <wrh1600@gmail.com>
2018-04-12 23:36:09 -0700

    First phase working. Working on different functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,141 +11,75 @@ var connection = mysql.createConnection({
     password: "root",
     database: "bamazonDB"
 });
-
-var purchase;
-
-
-function start(){
-    inquirer.prompt({
-        type: "list",
-        message: "What would you like to view?",
-        name: "answer",
-        choices: ["All inventory", "Electronics", "Furniture", "Apparel", "Camping/Outdoors", "Tools", "Exit"]
-    }).then(function(data){
-        switch(data.answer) {
-            case "All inventory":
-                showItems();
-                break;
-
-            case "Electronics":
-                showElectronics();
-                break;
-
-            case "Furniture":
-                showfurniture();
-                break;
-            
-            case "Apparel":
-                showApparel();
-                break;
-                
-            case "Camping/Outdoors":
-                showCampingOutdoors();
-                break;
-            
-            case "Tools":
-                showTools();
-                break;
-                
-            case "Exit":
-                console.log("\nThanks for shopping with Bamazon!\n")
-                setTimeout(function(){
-                    connection.end();
-                }, 1500);
-            
-                break;    
-        }
-    })
-}    
+   
 
 showItems();
 
 function showItems() {
-    connection.query("SELECT * FROM products", function (err, result, fields) {
-        if (err) throw err;
-        var productId = [];
-        for(i = 0; i < result.length; i++){
-            console.log("\n" + colors.green(result[i].id) + ") " + "Item: ".yellow + result[i].product_name  + "\n    Department: ".yellow + result[i].department + "\n    Price: ".yellow + "$"+ result[i].price + "\n-------------------".blue);
-            productId.push(result[i].id);
+    inquirer.prompt({
+        type: "list",
+        message: "What would you like to view?",
+        name: "answer",
+        choices: ["All inventory", "Electronics", "Furniture", "Apparel", "Camping/Outdoors", "Tools"]
+    }).then(function(data){
+        var query;
+        if(data.answer === "All inventory"){
+            query = "SELECT * FROM products"
         }
+        else{
+            query = "SELECT * FROM products WHERE department = " + "'" + data.answer.toLowerCase() + "'"
+        }
+        connection.query( query , function (err, result, fields) {
+            if (err) throw err;
+            var productId = [];
+            var stockQty = {};
+            for(i = 0; i < result.length; i++){
+            console.log("\n" + colors.green(result[i].id) + ") " + "Item: ".yellow + result[i].product_name  + "\n    Department: ".yellow + result[i].department + "\n    Price: ".yellow + "$"+ result[i].price + "\n    Stock: ".yellow +  result[i].stock_qty + "\n-------------------".blue);
+            productId.push(result[i].id.toString() + ") " + result[i].product_name);
+            stockQty[result[i].id] = (result[i].stock_qty);
+        }
+    })    
+
         inquirer.prompt([
             {
                 type: "list",
-                message: "Which item would you like to purchase?",
-                choices: productId,
-                name: "purchase"
+                message: "Which item would you like to buy?",
+                name: "purchase",
+                choices: productId
             },
             {
                 type: "input",
-                message: "How many do you want to purchase?",
+                message: "How many would you like to buy",
                 name: "count",
-                // validate: function(value) {
-                //     if (isNaN(value) === false) {
-                //       return true;
-                //     }
-                //     return false;
-                //   }
-            }
-        ]).then(function(data){
+                validate: function(value) {
+                    if (isNaN(value) === false) {
+                      return true;
+                    }
+                    return false;
+                  }
+            }    
 
+        ]).then(function(data){
+            if(data.count > stockQty[data.purchase[0]] || stockQty[data.purchase[0]] == 0 ){
+                console.log("\nInsufficient Quantity!".red);
+            }else{
+                    connection.query("UPDATE products SET ? WHERE ?",
+                [
+                    {
+                        stock_qty: stockQty[data.purchase[0]] - data.count 
+                    },
+                    {
+                        id: data.purchase[0]
+                    }
+                ]);
+                console.log("Purchase Complete!".green);
+            }
         })
-        // console.log(productId[9]);
     });
 }
 
-function showElectronics() {
-    connection.query("SELECT * FROM products WHERE department = 'electronics' ", function (err, result, fields) {
-        if (err) throw err;
-        for(i = 0; i < result.length; i++){
-        console.log("\nItem: " + result[i].product_name + "\n" + "Department: " + result[i].department + "\n" + "Price: $" + result[i].price + "\nStock: " + result[i].stock_qty + "\n----------------");
-        }
-      });
-    connection.end();
-}
 
-function showFurniture() {
-    connection.query("SELECT * FROM products WHERE department = 'furniture' ", function (err, result, fields) {
-        if (err) throw err;
-        for(i = 0; i < result.length; i++){
-        console.log("\nItem: " + result[i].product_name + "\n" + "Department: " + result[i].department + "\n" + "Price: $" + result[i].price + "\nStock: " + result[i].stock_qty + "\n----------------");
-        }
-      });
-    connection.end();
-}
 
-function showApparel() {
-    connection.query("SELECT * FROM products WHERE department = 'apparel' ", function (err, result, fields) {
-        if (err) throw err;
-        for(i = 0; i < result.length; i++){
-        console.log("\nItem: " + result[i].product_name + "\n" + "Department: " + result[i].department + "\n" + "Price: $" + result[i].price + "\nStock: " + result[i].stock_qty + "\n----------------");
-        }
-      });
-    connection.end();
-}
 
-function showCampingOutdoors() {
-    connection.query("SELECT * FROM products WHERE department = 'camping/outdoors' ", function (err, result, fields) {
-        if (err) throw err;
-        for(i = 0; i < result.length; i++){
-        console.log("\nItem: " + result[i].product_name + "\n" + "Department: " + result[i].department + "\n" + "Price: $" + result[i].price + "\nStock: " + result[i].stock_qty + "\n----------------");
-        }
-      });
-    connection.end();
-}
 
-function showTools() {
-    connection.query("SELECT * FROM products WHERE department = 'tools' ", function (err, result, fields) {
-        if (err) throw err;
-        for(i = 0; i < result.length; i++){
-        console.log("\nItem: " + result[i].product_name + "\n" + "Department: " + result[i].department + "\n" + "Price: $" + result[i].price + "\nStock: " + result[i].stock_qty + "\n----------------");
-        }
-      });
-    connection.end();
-}
-
-function checkStock(){
-    connection.query("SELECT * FROM products.stock_qty", function (err, result, fields){
-        console.log(result);
-    })
-}
 
