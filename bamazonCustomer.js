3d3e238ae874e0dc4ddb4ca855fd54d2e7f1579b
Wesley Hanson <wrh1600@gmail.com>
2018-04-13 21:49:09 -0700

    Finished 1st portion

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,37 +9,48 @@ var connection = mysql.createConnection({
     user: "root",
 
     password: "root",
-    database: "bamazonDB"
+    database: "bamazon_DB"
 });
+var query;
    
 
-showItems();
+start();
 
-function showItems() {
+function start(){
     inquirer.prompt({
         type: "list",
         message: "What would you like to view?",
         name: "answer",
-        choices: ["All inventory", "Electronics", "Furniture", "Apparel", "Camping/Outdoors", "Tools"]
+        choices: ["All inventory", "Electronics", "Furniture", "Apparel", "Camping/Outdoors", "Tools", "Exit"]
     }).then(function(data){
-        var query;
-        if(data.answer === "All inventory"){
+        if(data.answer === "Exit"){
+            console.log("\nThanks for shopping with Bamazon!\n".green);
+            connection.end();
+        }
+        else if(data.answer === "All inventory"){
             query = "SELECT * FROM products"
+            showItems();
         }
         else{
             query = "SELECT * FROM products WHERE department = " + "'" + data.answer.toLowerCase() + "'"
+            showItems();
         }
-        connection.query( query , function (err, result, fields) {
-            if (err) throw err;
-            var productId = [];
-            var stockQty = {};
-            for(i = 0; i < result.length; i++){
-            console.log("\n" + colors.green(result[i].id) + ") " + "Item: ".yellow + result[i].product_name  + "\n    Department: ".yellow + result[i].department + "\n    Price: ".yellow + "$"+ result[i].price + "\n    Stock: ".yellow +  result[i].stock_qty + "\n-------------------".blue);
-            productId.push(result[i].id.toString() + ") " + result[i].product_name);
-            stockQty[result[i].id] = (result[i].stock_qty);
-        }
-    })    
+        
+    }) 
+}       
 
+function showItems() {
+    
+    connection.query( query , function (err, result, fields) {
+        if (err) throw err;
+        var productId = [];
+        var stockQty = {};
+        for(i = 0; i < result.length; i++){
+        console.log("\n" + colors.green(result[i].id) + ") " + "Item: ".yellow + result[i].product_name  + "\n    Department: ".yellow + result[i].department + "\n    Price: ".yellow + "$"+ result[i].price + "\n    Stock: ".yellow +  result[i].stock_qty + "\n-------------------".blue);
+        productId.push(result[i].id.toString() + ") " + result[i].product_name);
+        stockQty[result[i].id] = (result[i].stock_qty);
+        }
+       
         inquirer.prompt([
             {
                 type: "list",
@@ -53,15 +64,18 @@ function showItems() {
                 name: "count",
                 validate: function(value) {
                     if (isNaN(value) === false) {
-                      return true;
+                        return true;
                     }
                     return false;
-                  }
+                    }
             }    
 
         ]).then(function(data){
             if(data.count > stockQty[data.purchase[0]] || stockQty[data.purchase[0]] == 0 ){
-                console.log("\nInsufficient Quantity!".red);
+                console.log("\nInsufficient Quantity!".red + "\n");
+                setTimeout (function(){
+                    start();
+                }, 1500)
             }else{
                     connection.query("UPDATE products SET ? WHERE ?",
                 [
@@ -72,10 +86,14 @@ function showItems() {
                         id: data.purchase[0]
                     }
                 ]);
-                console.log("Purchase Complete!".green);
+                console.log("\nPurchase Complete!".green + "\n");
+                setTimeout (function(){
+                    start();
+                }, 1500)
+               
             }
         })
-    });
+    });     
 }
 
 
